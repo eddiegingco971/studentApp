bcab9e6b47a995743d5f8ab06d98a77059388d72
Eddie Gingco <gingcoeddie971@gmail.com>
2022-09-14 12:32:49 +0800

    master-ed

---
+++ resources/views/pages/user-dashboard.blade.php
@@ -0,0 +1,34 @@
+@extends('base')
+
+@section('content')
+
+@include('nav')
+
+
+
+<div class="container">
+    @if (session('message'))
+    <div class="alert alert-success">
+        {{session('message')}}
+    </div>
+    @endif
+
+    @if (session('error'))
+    <div class="alert alert-danger">
+    {{session('error')}}
+    </div>
+    @endif 
+
+    <div class="row">
+        <div class="card mt-3">
+            <div class="card-header">
+                <div class="card-title">
+                    <h1>DASHBOARD</h1>
+                </div>
+                <div class="card-body">Welcoming</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+
+Route::get('/register', [AuthController::class, 'registerForm']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/login', [AuthController::class, 'loginForm']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/verification/{user}/{token}', [AuthController::class, 'verification']);
+
+Route::get('/user-dashboard', function(){
+    return view('pages.user-dashboard');
+})->middleware('auth');
